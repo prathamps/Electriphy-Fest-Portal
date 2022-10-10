e2be690fd0cdc03235cc5a29a6a21f63423ff60f
prathamps <pspratham16@gmail.com>
2022-10-10 09:17:40 +0530

    Deploying to heroku

--- components/Registration.jsx
+++ components/Registration.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react"
-import { db } from "../pages/firebase"
+import { db } from "./firebase"
 import { collection, addDoc } from "firebase/firestore"
 
 // Framer motion
